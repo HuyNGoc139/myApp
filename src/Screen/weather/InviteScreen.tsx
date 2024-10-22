6e0be11c222dfc46e8f9fb6424b549c7d333d6a8
Nguyen Quang Huy <huy13092002@gmail.com>
2024-10-22 14:02:14 +0700

    update

--- src/Screen/weather/InviteScreen.tsx
+++ src/Screen/weather/InviteScreen.tsx
@@ -18,13 +18,17 @@ import { useCallback, useEffect, useState } from 'react';
 import { Calendar, Location, SearchNormal } from 'iconsax-react-native';
 import debounce from 'lodash/debounce';
 import axios from 'axios';
-import { fetchLocation, fetchWeatherForeCast,fetchCurrentLocation } from '../../api/weather';
+import {
+  fetchLocation,
+  fetchWeatherForeCast,
+  fetchCurrentLocation,
+} from '../../api/weather';
 
 import { Weather } from '../../model/weather';
 import { weatherImages } from '../../utils/weatherImage';
 import { WeatherCondition } from '../../utils/weatherImage';
 import Geolocation from 'react-native-geolocation-service';
-import {PermissionsAndroid, Platform} from 'react-native';
+import { PermissionsAndroid, Platform } from 'react-native';
 
 interface Location {
   latitude: number;
@@ -34,9 +38,9 @@ interface Locations {
   name: string;
 }
 const InviteScreen = () => {
-  const[search,setSearch]=useState('')
-  const[showSearch,setShowSearch]=useState(false)
-  const[locations,setLocations]=useState<any[]>([])
+  const [search, setSearch] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
+  const [locations, setLocations] = useState<any[]>([]);
   const [currentLocation, setCurrentLocation] = useState<Location | null>(null); // lưu vị trí hiện tại
   const [address, setAddress] = useState<string | undefined>('');
   const [weather, setWeather] = useState<any>({});
@@ -44,35 +48,32 @@ const InviteScreen = () => {
     const fetchLocationWeather = async () => {
       const hasPermission = await requestLocationPermission();
       if (!hasPermission) return;
-  
+
       Geolocation.getCurrentPosition(
         async (position) => {
-          console.log('====================================');
-          console.log(position);
-          console.log('====================================');
           const { latitude, longitude } = position.coords;
           setCurrentLocation({ latitude, longitude });
 
           const data = await fetchCurrentLocation({
             latitude,
             longitude,
-            day: '7', 
+            day: '7',
           });
-  
+
           if (data) {
             setWeather(data);
           } else {
-            console.log("Không thể lấy dữ liệu thời tiết từ vị trí hiện tại");
+            console.log('Không thể lấy dữ liệu thời tiết từ vị trí hiện tại');
           }
         },
         (error) => {
-          console.error("Lỗi khi lấy vị trí:", error);
+          console.error('Lỗi khi lấy vị trí:', error);
         },
         { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
       );
     };
-  
-    fetchLocationWeather(); 
+
+    fetchLocationWeather();
   }, []);
 
   const requestLocationPermission = useCallback(async () => {
@@ -81,16 +82,16 @@ const InviteScreen = () => {
         const granted = await PermissionsAndroid.request(
           PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
           {
-            title: "Location Permission",
-            message: "This app needs access to your location.",
-            buttonNeutral: "Ask Me Later",
-            buttonNegative: "Cancel",
-            buttonPositive: "OK",
+            title: 'Location Permission',
+            message: 'This app needs access to your location.',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
           }
         );
         return granted === PermissionsAndroid.RESULTS.GRANTED;
       } catch (err) {
-        console.warn("Lỗi khi xin quyền vị trí:", err);
+        console.warn('Lỗi khi xin quyền vị trí:', err);
         return false;
       }
     }
@@ -111,18 +112,17 @@ const InviteScreen = () => {
     }
   };
 
-  const handleSearch = (val: string)=> {
-    if(val.length>2){
+  const handleSearch = (val: string) => {
+    if (val.length > 2) {
       fetchLocation({ cityName: val }).then((data) => {
-        setLocations(data)
+        setLocations(data);
         return; // Trả về một giá trị void
       });
-    }
-    else return
+    } else return;
   };
 
-  const handleTextDebounce=useCallback(debounce(handleSearch,1200),[])
-  const {current,location}=weather||{}
+  const handleTextDebounce = useCallback(debounce(handleSearch, 1200), []);
+  const { current, location } = weather || {};
 
   return (
     <ImageBackground
@@ -130,140 +130,249 @@ const InviteScreen = () => {
       style={{ flex: 1 }}
       resizeMode="cover"
     >
-        <HeaderComponent title="Weather" />
-        <View style={{ height: '7%', marginHorizontal: 16, position: 'relative', zIndex: 50 }}>
+      <HeaderComponent title="Weather" />
       <View
-        style={[
-          styles.row, 
-          { backgroundColor: showSearch ? 'rgba(255, 255, 255, 0.1)' : 'transparent' }
-        ]}
+        style={{
+          height: '7%',
+          marginHorizontal: 16,
+          position: 'relative',
+          zIndex: 50,
+        }}
       >
-        {showSearch ? (
-          <TextInput
-          onChangeText={handleTextDebounce}
-            placeholder="Search city"
-            placeholderTextColor="gray"
-            style={[styles.input, { color: 'rgba(255, 255, 255, 0.1)' }]}
-          />
-        ) : null}
-
-        <TouchableOpacity
-          onPress={() => setShowSearch(!showSearch)}
-          style={[styles.button, { backgroundColor: 'rgba(255, 255, 255, 0.1)' }]}
+        <View
+          style={[
+            styles.row,
+            {
+              backgroundColor: showSearch
+                ? 'rgba(255, 255, 255, 0.1)'
+                : 'transparent',
+            },
+          ]}
         >
-          <SearchNormal size={25} color="#999" />
-        </TouchableOpacity>
-      </View>
-      {
-      locations.length>0&&showSearch?(
-        <View style={{backgroundColor:'rgb(237, 237, 237)',marginHorizontal:20,borderRadius:25,paddingHorizontal:20}}>
-          {
-            locations.map((loc,index)=>{
-              return(
-                <TouchableOpacity key={index}  onPress={()=>handelLocation(loc)}
-                style={{padding:16,
-                borderBottomWidth:1,
-                borderBottomColor:'gray',
-                flexDirection:'row'
-                }}>
-                  <Location size="24" color="gray"/>
-                  <Text style={{color:'black',fontWeight:'500',fontSize:18,marginLeft:6}}>{loc?.name}, {loc?.country}</Text>
+          {showSearch ? (
+            <TextInput
+              onChangeText={handleTextDebounce}
+              placeholder="Search city"
+              placeholderTextColor="gray"
+              style={[styles.input, { color: 'rgba(255, 255, 255, 0.1)' }]}
+            />
+          ) : null}
+
+          <TouchableOpacity
+            onPress={() => setShowSearch(!showSearch)}
+            style={[
+              styles.button,
+              { backgroundColor: 'rgba(255, 255, 255, 0.1)' },
+            ]}
+          >
+            <SearchNormal size={25} color="#999" />
+          </TouchableOpacity>
+        </View>
+        {locations.length > 0 && showSearch ? (
+          <View
+            style={{
+              backgroundColor: 'rgb(237, 237, 237)',
+              marginHorizontal: 20,
+              borderRadius: 25,
+              paddingHorizontal: 20,
+            }}
+          >
+            {locations.map((loc, index) => {
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => handelLocation(loc)}
+                  style={{
+                    padding: 16,
+                    borderBottomWidth: 1,
+                    borderBottomColor: 'gray',
+                    flexDirection: 'row',
+                  }}
+                >
+                  <Location size="24" color="gray" />
+                  <Text
+                    style={{
+                      color: 'black',
+                      fontWeight: '500',
+                      fontSize: 18,
+                      marginLeft: 6,
+                    }}
+                  >
+                    {loc?.name}, {loc?.country}
+                  </Text>
                 </TouchableOpacity>
-              )
-            })
-          }
-        </View>
-      ):null
-    }
-    </View>
-    {/* SearchItem */}
-    
-    <View style={{flex:1, marginHorizontal:20,marginBottom:8,justifyContent:'space-around'}}>
-      {/* Name */}
-      <Text style={{color:'white',textAlign:'center',fontWeight:'700',fontSize:20}}>
-        {location?.name?location.name:"Ha Noi"}{', '}
-        <Text style={{fontWeight:'400',fontSize:18}}>
-        {location?.country?location.country:"Viet Nam"}
-      </Text>
-      </Text>
-
-      {/* Weather Image */}
-
-      <View style={{flexDirection:'row',justifyContent:'center'}}>
-      {current?.condition.icon?<Image style={{width:240,height:240}} source={weatherImages[current?.condition?.text as WeatherCondition]}/>
-        :<Image style={{width:240,height:240}} source={require('../../assets/weather/sun.png')}/>}
+              );
+            })}
+          </View>
+        ) : null}
       </View>
-      {/* Nhiet do */}
-      <View>
-      <Text style={{color:'white',textAlign:'center',fontWeight:'700',fontSize:60}}>
-        {current?.temp_c}&#176;
-      </Text>
-      <Text style={{color:'white',textAlign:'center',fontWeight:'400',fontSize:24}}>
-        {current?.condition.text}
-      </Text>
-      </View>
-      {/* other weather */}
-      <View style={{flexDirection:'row',justifyContent:'space-between',}}>
-        <View style={{flexDirection:'row',alignItems:'center'}}>
-         <Image style={{width:24,height:24,marginRight:6}} source={require('../../assets/iconweather/wind.png')}/>
-          <Text style={{color:'white',textAlign:'center',fontWeight:'400',fontSize:18}}>
-            {current?.wind_kph}km
-          </Text>
-        </View>
-        <View style={{flexDirection:'row',alignItems:'center'}}>
-          <Image style={{width:24,height:24,marginRight:6}} source={require('../../assets/iconweather/drop.png')}/>
-          <Text style={{color:'white',textAlign:'center',fontWeight:'400',fontSize:18}}>
-            {current?.humidity}%
-          </Text>
-        </View>
-        <View style={{flexDirection:'row',alignItems:'center'}}>
-          <Image style={{width:24,height:24,marginRight:6}} source={require('../../assets/iconweather/sun.png')}/>
-          <Text style={{color:'white',textAlign:'center',fontWeight:'400',fontSize:18}}>
-            6:05 AM
-          </Text>
-        </View>
-      </View>
+      {/* SearchItem */}
 
-      {/* weather next day */}
-      <View>
-        <View style={{flexDirection:'row',alignItems:'center',}}>
-        <Calendar size="24" color="white"/>
-        <Text style={{color:'white',textAlign:'center',marginLeft:8}}>
-        Daily forecast
-      </Text>
-        </View>
-        <ScrollView 
-        horizontal
-        contentContainerStyle={{paddingHorizontal:15}}
-        showsHorizontalScrollIndicator={false}
-        style={{marginTop:16}}
+      <View
+        style={{
+          flex: 1,
+          marginHorizontal: 20,
+          marginBottom: 8,
+          justifyContent: 'space-around',
+        }}
+      >
+        {/* Name */}
+        <Text
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            fontWeight: '700',
+            fontSize: 20,
+          }}
         >
-          {weather?.forecast?.forecastday.map((item: any,index: any)=>{
-            let date = new Date(item.date);
-            let options: Intl.DateTimeFormatOptions = { weekday: 'long' }; // Đặt kiểu chính xác cho options
-            let dayName = date.toLocaleDateString('en-US', options);
-            return(
-              <View key={index}
-              style={{backgroundColor:'rgba(255, 255, 255, 0.1)',
-          paddingVertical:10,
-          paddingHorizontal:24,
-          borderRadius:20,
-          justifyContent:'center',alignItems:'center',
-          marginRight:16,
-          }}>
-            <Image style={{width:52,height:52}} source={{uri:'http:'+item?.day?.condition?.icon}}/>
-            <Text style={{color:'white',fontSize:18,fontWeight:'600'}}>
-              {item?.day?.avgtemp_c}&#176;
-            </Text>
-            <Text style={{color:'white'}}>
-              {dayName}
+          {location?.name ? location.name : 'Ha Noi'}
+          {', '}
+          <Text style={{ fontWeight: '400', fontSize: 18 }}>
+            {location?.country ? location.country : 'Viet Nam'}
+          </Text>
+        </Text>
+
+        {/* Weather Image */}
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          {current?.condition.icon ? (
+            <Image
+              style={{ width: 240, height: 240 }}
+              source={
+                weatherImages[current?.condition?.text as WeatherCondition]
+              }
+            />
+          ) : (
+            <Image
+              style={{ width: 240, height: 240 }}
+              source={require('../../assets/weather/sun.png')}
+            />
+          )}
+        </View>
+        {/* Nhiet do */}
+        <View>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: '700',
+              fontSize: 60,
+            }}
+          >
+            {current?.temp_c}&#176;
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              fontWeight: '400',
+              fontSize: 24,
+            }}
+          >
+            {current?.condition.text}
+          </Text>
+        </View>
+        {/* other weather */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Image
+              style={{ width: 24, height: 24, marginRight: 6 }}
+              source={require('../../assets/iconweather/wind.png')}
+            />
+            <Text
+              style={{
+                color: 'white',
+                textAlign: 'center',
+                fontWeight: '400',
+                fontSize: 18,
+              }}
+            >
+              {current?.wind_kph}km
             </Text>
           </View>
-            )
-          })}
-        </ScrollView>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Image
+              style={{ width: 24, height: 24, marginRight: 6 }}
+              source={require('../../assets/iconweather/drop.png')}
+            />
+            <Text
+              style={{
+                color: 'white',
+                textAlign: 'center',
+                fontWeight: '400',
+                fontSize: 18,
+              }}
+            >
+              {current?.humidity}%
+            </Text>
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Image
+              style={{ width: 24, height: 24, marginRight: 6 }}
+              source={require('../../assets/iconweather/sun.png')}
+            />
+            <Text
+              style={{
+                color: 'white',
+                textAlign: 'center',
+                fontWeight: '400',
+                fontSize: 18,
+              }}
+            >
+              6:05 AM
+            </Text>
+          </View>
+        </View>
+
+        {/* weather next day */}
+        <View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Calendar size="24" color="white" />
+            <Text
+              style={{ color: 'white', textAlign: 'center', marginLeft: 8 }}
+            >
+              Daily forecast
+            </Text>
+          </View>
+          <ScrollView
+            horizontal
+            contentContainerStyle={{ paddingHorizontal: 15 }}
+            showsHorizontalScrollIndicator={false}
+            style={{ marginTop: 16 }}
+          >
+            {weather?.forecast?.forecastday.map((item: any, index: any) => {
+              let date = new Date(item.date);
+              let options: Intl.DateTimeFormatOptions = { weekday: 'long' }; // Đặt kiểu chính xác cho options
+              let dayName = date.toLocaleDateString('en-US', options);
+              return (
+                <View
+                  key={index}
+                  style={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                    paddingVertical: 10,
+                    paddingHorizontal: 24,
+                    borderRadius: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginRight: 16,
+                  }}
+                >
+                  <Image
+                    style={{ width: 52, height: 52 }}
+                    source={{ uri: 'http:' + item?.day?.condition?.icon }}
+                  />
+                  <Text
+                    style={{ color: 'white', fontSize: 18, fontWeight: '600' }}
+                  >
+                    {item?.day?.avgtemp_c}&#176;
+                  </Text>
+                  <Text style={{ color: 'white' }}>{dayName}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
       </View>
-    </View>
     </ImageBackground>
   );
 };
--- src/api/weather.js
+++ src/api/weather.js
@@ -1,28 +1,30 @@
-import axios from "axios"
+import axios from 'axios';
 
-const API_KEY ='68733361387a4cdd9b870705242110'
-const forecastEndpoint=params=>`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.city}&days=${params.day}&aqi=no&alerts=no`
-const locationEndpoint=params=>`https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${params.cityName}`
+const API_KEY = '68733361387a4cdd9b870705242110';
+const forecastEndpoint = (params) =>
+  `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.city}&days=${params.day}&aqi=no&alerts=no`;
+const locationEndpoint = (params) =>
+  `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${params.cityName}`;
 export const fetchCurrentLocation = (params) => {
-    const endpoint = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.latitude},${params.longitude}&days=${params.day}`;
-    return apiCall(endpoint);
+  const endpoint = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.latitude},${params.longitude}&days=${params.day}`;
+  return apiCall(endpoint);
 };
-const apiCall=async(endpoint)=>{
-    const options={
-        method:'GET',
-        url:endpoint
-    }
-    try {
-        const response=axios.request(options);
-        return (await response).data
-    } catch (error) {
-        console.log('err:',error)
-        return null
-    }
-}
-export const fetchWeatherForeCast=params=>{
-    return apiCall(forecastEndpoint(params))
-}
-export const fetchLocation=params=>{
-    return apiCall(locationEndpoint(params))
-}
+const apiCall = async (endpoint) => {
+  const options = {
+    method: 'GET',
+    url: endpoint,
+  };
+  try {
+    const response = axios.request(options);
+    return (await response).data;
+  } catch (error) {
+    console.log('err:', error);
+    return null;
+  }
+};
+export const fetchWeatherForeCast = (params) => {
+  return apiCall(forecastEndpoint(params));
+};
+export const fetchLocation = (params) => {
+  return apiCall(locationEndpoint(params));
+};
